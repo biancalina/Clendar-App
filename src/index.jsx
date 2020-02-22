216eb71dd98e528c85388219a5497b8211d9f0ea
biancalina <bianca.alyna30@gmail.com>
2020-02-22 19:24:06 +0000

    Start working on my calendar app

---
+++ src/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { render } from 'react-dom'
+
+const calendarApp = document.querySelector('#calendar-app')
+const CalendarApp = () => {
+  return <div>Hello</div>
+}
+
+render(
+  <CalendarApp/>,
+  calendarApp
+)
